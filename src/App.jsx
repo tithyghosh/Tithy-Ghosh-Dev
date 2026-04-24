b8bf238c85f2ef22a78a5a9a6efb8c7ec66eacaa
Tithy Ghosh <tithy.ghosh.07@gmail.com>
2026-04-25 01:51:41 +0600

    Made structure layout and made the navbar section

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Features/Navbar/Navbar'
+
+const App = () => {
+  return (
+    <div className='hero-glow'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
